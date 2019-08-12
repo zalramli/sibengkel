576613949cf56e037bdc05127eb5fe0f8088da6a
zalramli <github.little@gmail.com>
2019-08-12 16:55:34 +0700

    custom folder

---
+++ koneksi/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+	$koneksi = mysqli_connect("localhost","root","","db_wisata");
+	if ($koneksi == false) {
+		echo "Koneksi Anda gagal karena ".mysqli_connect_error();
+	}
+	else{
+		 //echo "Haloo Berhasilll";
+	}
+
+	// function kode($field,$tabel,$digit,$kode){
+	// $data = mysqli_fetch_array(mysqli_query($koneksi,"SELECT MAX(RIGHT($field,$digit)) FROM $tabel"));
+	// $id = (int)$data[0]+1;
+	// $id_baru = $kode.sprintf('%0'.$digit.'s',$id);
+	// echo $id_baru;
+	// }
+?>
