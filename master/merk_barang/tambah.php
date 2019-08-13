565124ebaaa35daed95b73052f968e6a792fb75f
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-14 00:53:50 +0700

    perbaikan merk barang
    perbaikan merk barang

--- master/merk_barang/tambah.php
+++ master/merk_barang/tambah.php
@@ -1,45 +1,45 @@
-<?php 
-  // Membuat Kode otomatis
-    $sql = mysqli_query($koneksi,"SELECT max(kode_merk) FROM merk");
-    $kode_faktur = mysqli_fetch_array($sql);
-    if($kode_faktur){
-      $nilai = substr($kode_faktur[0], 1);
-      $kode = (int) $nilai;
-      //tambahkan sebanyak + 1
-      $kode = $kode + 1;
-      $auto_kode = "M" .str_pad($kode, 2, "0",  STR_PAD_LEFT);
-    } else {
-      $auto_kode = "M01";
-    }
+<?php
+// Membuat Kode otomatis
+$sql = mysqli_query($koneksi, "SELECT max(kode_merk) FROM merk");
+$kode_faktur = mysqli_fetch_array($sql);
+if ($kode_faktur) {
+  $nilai = substr($kode_faktur[0], 1);
+  $kode = (int) $nilai;
+  //tambahkan sebanyak + 1
+  $kode = $kode + 1;
+  $auto_kode = "M" . str_pad($kode, 2, "0",  STR_PAD_LEFT);
+} else {
+  $auto_kode = "M01";
+}
 
-    // Ketika tombil simpan di Klik
-    if (isset($_POST['simpan'])) {
-      $nama_merk = $_POST['nama_merk'];
-      $query = mysqli_query($koneksi,"INSERT INTO merk (kode_merk,nama_merk) VALUES ('$auto_kode','$nama_merk') ");
-      if($query){
-        echo "<script>window.location = 'gudang.php?halaman=v_merkBarang'</script>";
-      }
-    }
+// Ketika tombil simpan di Klik
+if (isset($_POST['simpan'])) {
+  $nama_merk = $_POST['nama_merk'];
+  $query = mysqli_query($koneksi, "INSERT INTO merk (kode_merk,nama_merk) VALUES ('$auto_kode','$nama_merk') ");
+  if ($query) {
+    echo "<script>window.location = 'gudang.php?halaman=v_merkBarang'</script>";
+  }
+}
 ?>
 <div class="form-element-list">
-    <div class="basic-tb-hd">
-        <h2>Form Tambah Merk Barang</h2>
-    </div>
+  <div class="basic-tb-hd">
+    <h2>Form Tambah Merk Barang</h2>
+  </div>
+
+  <form action="" method="post">
     <div class="row">
-        <form action="" method="post">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <label for="">Nama Merk Barang</label>
-            <div class="form-group">
-                <div class="nk-int-st">
-                    <input type="text" name="nama_merk" class="form-control" placeholder="Isi form nama merk barang">
-                </div>
-            </div>
+      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <label for="">Nama Merk Barang</label>
+        <div class="form-group">
+          <div class="nk-int-st">
+            <input type="text" name="nama_merk" class="form-control" placeholder="Isi form nama merk barang">
+          </div>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-            <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
-            <a href="gudang.php?halaman=v_merkBarang" class="btn btn-danger">Kembali</a>
-        </div>
-        </form>
+      </div>
     </div>
-    
+    <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
+    <a href="gudang.php?halaman=v_merkBarang" class="btn btn-danger">Kembali</a>
+  </form>
+
+
 </div>
--- master/merk_barang/tampil.php
+++ master/merk_barang/tampil.php
@@ -1,16 +1,17 @@
 <?php
-    //Proses menghapus data
-    if ($_GET['hapus']) {
+//Proses menghapus data
+if (isset($_GET['hapus'])) {
     $id = $_GET['hapus'];
-    $query_hapus = mysqli_query($koneksi,"DELETE FROM merk WHERE kode_merk='$id'");
-      if ($query_hapus) {
-          echo "<script>window.location = 'gudang.php?halaman=v_merkBarang'</script>";
-      }
+    $query_hapus = mysqli_query($koneksi, "DELETE FROM merk WHERE kode_merk='$id'");
+    if ($query_hapus) {
+        echo "<script>window.location = 'gudang.php?halaman=v_merkBarang'</script>";
     }
- ?>
+}
+?>
 <div class="data-table-list">
     <div class="basic-tb-hd">
-        <h2>Merk BARANG</h2>
+        <h2>Merk Barang</h2>
+        <br>
         <a href="gudang.php?halaman=add_merkBarang" class="btn btn-success notika-btn-success">Tambah Data</a>
     </div>
     <div class="table-responsive">
@@ -24,25 +25,25 @@
             </thead>
             <tbody>
                 <?php
-                $query = mysqli_query($koneksi,"SELECT kode_merk,nama_merk FROM merk");
+                $query = mysqli_query($koneksi, "SELECT kode_merk,nama_merk FROM merk");
                 foreach ($query as $data) {
-                 ?>
-                  <tr>
-                      <td><?= $data['kode_merk']?></td>
-                      <td><?= $data['nama_merk']?></td>
-                      <td>
-                        <a href="?halaman=edit_merkBarang&id=<?= $data['kode_merk']; ?>"" class="btn btn-primary">Edit</a>
+                    ?>
+                <tr>
+                    <td><?= $data['kode_merk'] ?></td>
+                    <td><?= $data['nama_merk'] ?></td>
+                    <td>
+                        <a href="?halaman=edit_merkBarang&id=<?= $data['kode_merk']; ?>"" class=" btn btn-primary">Edit</a>
                         <a onclick="return confirm('Yakin ingin menghapus data ?')" href="?halaman=v_merkBarang&hapus=<?= $data['kode_merk']; ?>" class="btn btn-danger">Hapus</a>
-                      </td>
-                  </tr>
-                  <?php } ?>
+                    </td>
+                </tr>
+                <?php } ?>
             </tbody>
             <tfoot>
-            <tr>
+                <tr>
                     <th>Kode Merk Barang</th>
                     <th>Nama Merk Barang</th>
                     <th>Aksi</th>
-            </tr>
+                </tr>
             </tfoot>
         </table>
     </div>
