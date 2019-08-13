5c00f17c54e7584174bd87f4aa08d3e0f9b4a460
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-13 23:41:31 +0700

    navbar gudang repair
    navbar gudang repair

--- _partial/navbar_mobile_gudang.php
+++ _partial/navbar_mobile_gudang.php
@@ -1,52 +1,36 @@
 <div class="mobile-menu-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="mobile-menu">
-                        <nav id="dropdown">
-                            <ul class="mobile-menu-nav">
-                                <li><a data-toggle="collapse" data-target="#Charts" href="#">Home</a>
-                                    <ul class="collapse dropdown-header-top">
-                                        <li><a href="index.html">Dashboard One</a></li>
-                                        <li><a href="index-2.html">Dashboard Two</a></li>
-                                        <li><a href="index-3.html">Dashboard Three</a></li>
-                                        <li><a href="index-4.html">Dashboard Four</a></li>
-                                        <li><a href="analytics.html">Analytics</a></li>
-                                        <li><a href="widgets.html">Widgets</a></li>
-                                    </ul>
-                                </li>
-                                <li><a data-toggle="collapse" data-target="#demoevent" href="#">Order Barang/Sparepart</a>
-                                    <ul id="demoevent" class="collapse dropdown-header-top">
-                                        <li><a href="inbox.html">Inbox</a></li>
-                                        <li><a href="view-email.html">View Email</a></li>
-                                        <li><a href="compose-email.html">Compose Email</a></li>
-                                    </ul>
-                                </li>
-                                <li><a data-toggle="collapse" data-target="#democrou" href="#">Laporan</a>
-                                    <ul id="democrou" class="collapse dropdown-header-top">
-                                        <li><a href="animations.html">Animations</a></li>
-                                        <li><a href="google-map.html">Google Map</a></li>
-                                        <li><a href="data-map.html">Data Maps</a></li>
-                                        <li><a href="code-editor.html">Code Editor</a></li>
-                                        <li><a href="image-cropper.html">Images Cropper</a></li>
-                                        <li><a href="wizard.html">Wizard</a></li>
-                                    </ul>
-                                </li>
-                                <li><a data-toggle="collapse" data-target="#demolibra" href="#">Data Barang</a>
-                                    <ul id="demolibra" class="collapse dropdown-header-top">
-                                        <li><a href="gudang.php?halaman=v_daftarBarang">Daftar Barang</a>
-                                        </li>
-                                        <li><a href="gudang.php?halaman=v_jenisBarang">Jenis Barang</a>
-                                        </li>
-                                        <li><a href="gudang.php?halaman=v_merkBarang">Merk Barang</a>
-                                        <li><a href="gudang.php?halaman=v_satuanBarang">Satuan Barang</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="mobile-menu">
+                    <nav id="dropdown">
+                        <ul class="mobile-menu-nav">
+
+                            <li><a href="gudang.php?halaman=dashboard">Dashboard</a>
+                            </li>
+
+                            <li><a href="">Order Barang/Sparepart</a>
+                            </li>
+
+                            <li><a data-toggle="collapse" data-target="#demolibra" href="#">Data Barang</a>
+                                <ul id="demolibra" class="collapse dropdown-header-top">
+                                    <li><a href="gudang.php?halaman=v_daftarBarang">Daftar Barang</a>
+                                    </li>
+                                    <li><a href="gudang.php?halaman=v_jenisBarang">Jenis Barang</a>
+                                    </li>
+                                    <li><a href="gudang.php?halaman=v_merkBarang">Merk Barang</a>
+                                    <li><a href="gudang.php?halaman=v_satuanBarang">Satuan Barang</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li><a href="">Laporan</a>
+                            </li>
+
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
     </div>
+</div>
