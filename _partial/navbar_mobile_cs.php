635667aacd56337f51a01e337061817aee5dc43a
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-14 01:54:29 +0700

    menghaspus footer, chat , dan repair format code

--- _partial/navbar_mobile_cs.php
+++ _partial/navbar_mobile_cs.php
@@ -1,23 +1,24 @@
 <div class="mobile-menu-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <div class="mobile-menu">
-                        <nav id="dropdown">
-                            <ul class="mobile-menu-nav">
-                                <li><a href="cs.php?halaman=dashboard">Dashboard</a>
-                                </li>
-                                
-                                <li><a href="cs.php?halaman=v_customer">Data Customer</a>
-                                </li>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="mobile-menu">
+                    <nav id="dropdown">
+                        <ul class="mobile-menu-nav">
 
-                                 <li><a href="">Laporan</a>
-                                </li>
+                            <li><a href="cs.php?halaman=dashboard">Dashboard</a>
+                            </li>
 
-                            </ul>
-                        </nav>
-                    </div>
+                            <li><a href="cs.php?halaman=v_customer">Data Customer</a>
+                            </li>
+
+                            <li><a href="">Laporan</a>
+                            </li>
+
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
     </div>
+</div>
