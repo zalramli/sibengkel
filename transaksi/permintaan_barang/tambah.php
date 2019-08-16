bdc8a2c3ee2359050d4d206aa17f7db63f640235
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-17 04:16:36 +0700

    Update tambah.php
    masih append

--- transaksi/permintaan_barang/tambah.php
+++ transaksi/permintaan_barang/tambah.php
@@ -7,7 +7,7 @@
       <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
         <div class="form-group">
           <div class="bootstrap-select fm-cmp-mg">
-            <select name="kode_barang" class="selectpicker" data-live-search="true" required="">
+            <select id="cari_kode_barang" name="kode_barang" class="selectpicker" data-live-search="true" required="">
 
               <option value="">Cari Barang</option>
 
@@ -26,63 +26,116 @@
       </div>
       <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
         <div class="form-group">
-          <button id="" name="" class="btn btn-primary">+</button>
-        </div>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <div class="form-group">
-          <button id="" name="" class="btn btn-primary">Simpan Pemesanan</button>
+          <button id="add_more" name="add_more" class="btn btn-primary">+</button>
         </div>
       </div>
     </div>
   </div>
 
-  <form action="" method="post">
+  <div class="row">
+    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+      <label>No</label>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+      <label>Kode Barang</label>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+      <label>Nama Barang</label>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+      <label>Jumlah Barang</label>
+    </div>
+  </div>
 
-    <div class="row">
-      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-        <label>No</label>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <label>Kode Barang</label>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <label>Nama Barang</label>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <label>Jumlah Pesan</label>
-      </div>
+  <form id="transaksi_form" method="post">
+    <div id="span_product_details">
+      <!-- disini isi detail -->
     </div>
 
-    <div id="detail_list">
-
-
-    </div>
-
-    <div class="row">
-      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-        <p>1</p>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <p>BR0001</p>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <p>Oli Federal</p>
-      </div>
-      <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-        <div class="form-group">
-          <div class="nk-int-st">
-            <input type="number" name="stock_limit" class="form-control" placeholder="Isi form Jumlah Pesan" required="" max="32000" oninvalid="this.setCustomValidity('Wajib Diisi')" oninput="setCustomValidity('')">
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-        <button id="" name="" class="btn btn-primary">-</button>
-      </div>
-    </div>
+    <button id="action" type="submit" class="btn btn-primary mr-2">Simpan Pemesanan</button>
 
   </form>
 </div>
 
 <!-- untuk ajax -->
 <script src="assets/template2/js/vendor/jquery-3.3.1.min.js"></script>
+
+
+<!-- script logika -->
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('#span_product_details').html('');
+    var count1 = -1;
+    var kode_barang = "";
+
+    function add_row_obat(count1 = '', kode_barang, nama_barang) {
+
+      $('#span_product_details').append(`
+
+        <br />
+        <div id="row` + count1 + `" class="row">
+          <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+            <p>` + count1 + `</p>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <input type="text" class="form-control" id="kode_barang` + count1 + `" name="kode_barang[]" readonly="" value="` + kode_barang + `">
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <input type="text" class="form-control" id="nama_barang` + count1 + `" name="nama_barang[]" readonly="" value="` + nama_barang + `">
+          </div>
+          <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+            <div class="form-group">
+              <div class="nk-int-st">
+                <input type="number" id="jumlah_barang` + count1 + `" name="jumlah_barang[]" class="form-control" placeholder="Isi form Jumlah Pesan" required="" max="32000" oninvalid="this.setCustomValidity('Wajib Diisi')" oninput="setCustomValidity('')">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+            <button id="` + count1 + `" name="remove" class="remove btn btn-primary">-</button>
+          </div>
+        </div>
+        
+      `);
+    }
+
+    // menambah detail pemasokan
+    $(document).on('click', '#add_more', function() {
+
+      var cari_kode_barang = document.getElementById("cari_kode_barang");
+      var value = cari_kode_barang.options[cari_kode_barang.selectedIndex].value;
+      var value2 = cari_kode_barang.options[cari_kode_barang.selectedIndex].text;
+
+      count1 = count1 + 1;
+      add_row_obat(count1, value,value2);
+
+    });
+    // menambah detail pemasokan
+
+    // mengurangi detail pemasokan
+    $(document).on('click', '.remove', function() {
+      var row_no = $(this).attr("id");
+      $('#row' + row_no).remove();
+    });
+    // mengurangi detail pemasokan
+
+    // tambah ke database
+    $(document).on('submit', '#transaksi_form', function(event) {
+      
+      event.preventDefault();
+        $('#action').attr('disabled', 'disabled');
+        var form_data = $(this).serialize();
+        $.ajax({
+          url: "",
+          method: "POST",
+          data: form_data,
+          success: function(data) {
+            location.reload();
+          }
+        });
+
+    });
+    // tambah ke database
+
+  });
+</script>
+<!-- script logika -->
