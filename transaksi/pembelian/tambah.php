96db306e82d1d802d595323b95ffd470e137f36a
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-18 02:56:55 +0700

    pemebenaran form
    pemebenaran form

--- transaksi/pembelian/tambah.php
+++ transaksi/pembelian/tambah.php
@@ -64,78 +64,127 @@ if (isset($_POST['simpan'])) {
 <div class="form-element-list">
     <form id="transaksi_form" method="post">
 
-        <div class="basic-tb-hd">
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                    <div class="form-group">
-                        <div class="bootstrap-select fm-cmp-mg">
-                            <select id="cari_kode_suplier" required="" class="selectpicker" data-live-search="true">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
 
-                                <option value="">Cari Suplier</option>
+                <div class="basic-tb-hd">
 
-                                <?php
-                                $query_suplier = mysqli_query($koneksi, "SELECT * FROM suplier ORDER BY kode_suplier ASC");
-                                while ($data_suplier = mysqli_fetch_array($query_suplier)) {
-                                    ?>
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                            <div class="form-group">
+                                <div class="bootstrap-select fm-cmp-mg">
+                                    <select id="cari_kode_suplier" required="" class="selectpicker" data-live-search="true">
 
-                                <option value="<?= $data_suplier['kode_suplier'] ?>"><?= $data_suplier['nama_suplier'] ?></option>
+                                        <option value="">Cari Suplier</option>
 
-                                <?php } ?>
+                                        <?php
+                                        $query_suplier = mysqli_query($koneksi, "SELECT * FROM suplier ORDER BY kode_suplier ASC");
+                                        while ($data_suplier = mysqli_fetch_array($query_suplier)) {
+                                            ?>
 
-                            </select>
+                                        <option value="<?= $data_suplier['kode_suplier'] ?>"><?= $data_suplier['nama_suplier'] ?></option>
+
+                                        <?php } ?>
+
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
 
+                <div class="row">
+                    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                        <label>No</label>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <label>Kode Barang</label>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <label>Nama Barang</label>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                        <label>Jumlah Barang</label>
+                    </div>
                 </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                    <button onclick="return confirm('Yakin ingin Mengirim data Permintaan ?')" id="action" type="submit" name="simpan" class="btn btn-primary mr-2">Simpan Pemesanan</button>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                    <a href="kasir.php?halaman=v_transaksi_pembelian" class="btn btn-success notika-btn-success">Kembali</a>
-                </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-                <label>No</label>
+                <div id="span_product_details">
+                    <!-- disini isi detail -->
+                    <?php
+                    $index = 0;
+                    foreach ($query_tampil_detail as $data) {
+                        $index++;
+                        ?>
+                    <br />
+                    <div id="row" class="row">
+                        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                            <p><?= $index ?></p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                            <input type="text" class="form-control" id="kode_barang<?= $index ?>" name="kode_barang[]" readonly="" value="<?= $data['kode_barang'] ?>">
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                            <input type="text" class="form-control" id="nama_barang<?= $index ?>" name="nama_barang[]" readonly="" value="<?= $data['nama_barang'] ?>">
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <input type="text" class="form-control" id="jumlah_barang<?= $index ?>" name="jumlah_barang[]" readonly="" value="<?= $data['jumlah_barang'] ?>">
+                        </div>
+                    </div>
+                    <?php } ?>
+                </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <label>Kode Barang</label>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <label>Nama Barang</label>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <label>Jumlah Barang</label>
-            </div>
-        </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                <div class="contact-inner">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="basic-tb-hd">
+                                <h2 class="text-center">Form Pembayaran</h2>
+                            </div>
+                            <table width="100%">
+                                <tr>
+                                    <td width="60%">
+                                        <h5>Sub Total</h5>
+                                    </td>
+                                    <td width="40%" style="text-align: right;">
+                                        <h5>900.000</h5>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="padding-top: 9px;">
+                                        <h5>Potongan Harga</h5>
+                                    </td>
+                                    <td style="text-align: right; padding-top: 5px"><input type="text" class="form-control"></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div style="margin-top: 30px;" class="contact-inner">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <table width="100%">
+                                <tr>
+                                    <td style="padding-top: 15px;" width="60%">
+                                        <h5>Bayar</h5>
+                                    </td>
+                                    <td width="40%" style="text-align: right;"><input type="text" class="form-control"></td>
+                                </tr>
+                                <tr>
+                                    <td style="padding: 20px 0px;">
+                                        <h5>Kembalian</h5>
+                                    </td>
+                                    <td style="text-align: right; padding: 20px 0px;">
+                                        <h5>900.000</h5>
+                                    </td>
+                                </tr>
 
-        <div id="span_product_details">
-            <!-- disini isi detail -->
-            <?php
-            $index = 0;
-            foreach ($query_tampil_detail as $data) {
-                $index++;
-                ?>
-            <br />
-            <div id="row" class="row">
-                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-                    <p><?= $index ?></p>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                    <input type="text" class="form-control" id="kode_barang<?= $index ?>" name="kode_barang[]" readonly="" value="<?= $data['kode_barang'] ?>">
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                    <input type="text" class="form-control" id="nama_barang<?= $index ?>" name="nama_barang[]" readonly="" value="<?= $data['nama_barang'] ?>">
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                    <input type="text" class="form-control" id="jumlah_barang<?= $index ?>" name="jumlah_barang[]" readonly="" value="<?= $data['jumlah_barang'] ?>">
+                            </table>
+                            <button onclick="return confirm('Yakin ingin Melakukan Pemasokan ?')" id="action" type="submit" name="simpan" class="btn btn-primary mr-2">Simpan Pemasokan</button>
+                            <a href="kasir.php?halaman=v_transaksi_pembelian" class="btn btn-success notika-btn-success">Kembali</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <?php } ?>
         </div>
 
     </form>
