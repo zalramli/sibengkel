9cc2d0ef2838424944ed2e0ffd7f14971fdb7d9f
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-18 05:27:07 +0700

    menghitung nilai kembalian
    menghitung nilai kembalian

--- transaksi/pembelian/tambah.php
+++ transaksi/pembelian/tambah.php
@@ -3,7 +3,15 @@ if (isset($_GET['id'])) {
     // mengambil ID kode_permintaan
     $id = $_GET['id'];
 
+    // query ambil tabel detail permintaan
     $query_tampil_detail = mysqli_query($koneksi, "SELECT * FROM detail_permintaan JOIN barang USING(kode_barang) JOIN merk USING(kode_merk) JOIN satuan USING(kode_satuan) JOIN jenis_barang USING(kode_jenis) WHERE kode_permintaan='$id' ORDER BY kode_detail_permintaan ASC");
+
+    // query ambil total / sub total
+    $ambil_sub_total = mysqli_query($koneksi, "SELECT SUM(sub_total_harga) AS sub_total FROM detail_permintaan WHERE kode_permintaan='$id'");
+
+    foreach ($ambil_sub_total as $ambil_data) {
+        $sub_total =  $ambil_data['sub_total'];
+    }
 }
 ?>
 
@@ -146,14 +154,14 @@ if (isset($_POST['simpan'])) {
                                         <h5>Sub Total</h5>
                                     </td>
                                     <td width="40%" style="text-align: right;">
-                                        <h5>900.000</h5>
+                                        <input type="number" class="form-control" id="sub_total_hrg" readonly="" value="<?= $sub_total ?>">
                                     </td>
                                 </tr>
                                 <tr>
                                     <td style="padding-top: 9px;">
                                         <h5>Potongan Harga</h5>
                                     </td>
-                                    <td style="text-align: right; padding-top: 5px"><input type="text" class="form-control"></td>
+                                    <td style="text-align: right; padding-top: 5px"><input type="number" class="form-control" id="potongan" max="9999999999" onkeyup="update()" onchange="update()"></td>
                                 </tr>
                             </table>
                         </div>
@@ -167,14 +175,14 @@ if (isset($_POST['simpan'])) {
                                     <td style="padding-top: 15px;" width="60%">
                                         <h5>Bayar</h5>
                                     </td>
-                                    <td width="40%" style="text-align: right;"><input type="text" class="form-control"></td>
+                                    <td width="40%" style="text-align: right;"><input type="number" class="form-control" id="bayar" required="" max="9999999999" oninvalid="this.setCustomValidity('Bayar Wajib Diisi')" oninput="setCustomValidity('')" onkeyup="update()" onchange="update()"></td>
                                 </tr>
                                 <tr>
                                     <td style="padding: 20px 0px;">
                                         <h5>Kembalian</h5>
                                     </td>
                                     <td style="text-align: right; padding: 20px 0px;">
-                                        <h5>900.000</h5>
+                                        <input type="number" class="form-control" id="kembalian" readonly="">
                                     </td>
                                 </tr>
 
@@ -192,3 +200,29 @@ if (isset($_POST['simpan'])) {
 
 <!-- untuk ajax -->
 <script src="assets/template2/js/vendor/jquery-3.3.1.min.js"></script>
+
+<script>
+    // Menghitung kembalian
+    function update() {
+        var sub_total_hrg = document.getElementById("sub_total_hrg");
+        var potongan = document.getElementById("potongan");
+        var bayar = document.getElementById("bayar");
+        var kembalian = document.getElementById("kembalian");
+
+        // cek apakah kosong
+        if (potongan.value.length == 0) {
+            potongan.value = 0;
+        }
+
+        // parsing dan perhitungan
+        var v_total = parseFloat(sub_total_hrg.value) - parseFloat(potongan.value);
+        var v_bayar = parseFloat(bayar.value);
+
+        if (v_bayar >= v_total) {
+            kembalian.value = bayar.value - v_total;
+        } else {
+            kembalian.value = null;
+        }
+    }
+    // Menghitung kembalian
+</script>
