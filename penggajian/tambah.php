d8dcf6a54220eba7798edefb5f165f7205d1f74c
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-08-19 04:27:57 +0700

    form penggajian
    form penggajian

--- penggajian/tambah.php
+++ penggajian/tambah.php
@@ -1,129 +1,177 @@
 <?php
 // Ketika tombil simpan di Klik
-if (isset($_POST['simpan'])) {
 
-  // validasi apakah ada detail
-  if (isset($_POST['kode_barang'])) {
-
-    // Membuat Kode otomatis
-    $sql = mysqli_query($koneksi, "SELECT max(kode_permintaan) FROM permintaan_barang");
-    $kode_faktur = mysqli_fetch_array($sql);
-    if ($kode_faktur) {
-      $nilai = substr($kode_faktur[0], 2);
-      $kode = (int) $nilai;
-      //tambahkan sebanyak + 1
-      $kode = $kode + 1;
-      $auto_kode = "PB" . str_pad($kode, 6, "0",  STR_PAD_LEFT);
-    } else {
-      $auto_kode = "PB000001";
-    }
-
-    // mengupdate data tgl_last_log_in di database
-    date_default_timezone_set('Asia/Jakarta');
-    $now = date('Y-m-d H:i:s');
-
-    // data table permintaan
-    $kode_permintaan = $auto_kode;
-    $tgl_permintaan = $now;
-    $status = 0;
-
-    // proses input data tramsaksi ke dalam database
-    $query = mysqli_query($koneksi, "INSERT INTO permintaan_barang VALUES ('$kode_permintaan','$tgl_permintaan','$status') ");
-    if ($query) {
-
-      // proses input data detail tramsaksi ke dalam database
-      for ($i = 0; $i < count($_POST['kode_barang']); $i++) {
-
-        // data - data
-        $kode_barang = $_POST['kode_barang'][$i];
-        $jumlah_barang = $_POST['jumlah_barang'][$i];
-
-        // ambil harga pokok
-        $sql_ambil = mysqli_query($koneksi, "SELECT harga_pokok FROM barang WHERE kode_barang='$kode_barang'");
-        foreach ($sql_ambil as $data_ambil) {
-          $harga_pokok = $data_ambil['harga_pokok'];
-        }
-
-        // sub total
-        $sub_total_harga = $jumlah_barang * $harga_pokok;
-
-        $query_detail = mysqli_query($koneksi, "INSERT INTO detail_permintaan (kode_permintaan,kode_barang,jumlah_barang,sub_total_harga) VALUES ('$kode_permintaan','$kode_barang','$jumlah_barang','$sub_total_harga') ");
-      }
-
-      // validasi dan link
-      if ($query_detail) {
-        echo "<script>alert('Data Berhasil Ditambahkan'); window.location = 'admin.php?halaman=v_penggajian'</script>";
-      } else {
-        echo "<script>alert('Terjadi Kesalahan Input Database'); window.location = 'admin.php?halaman=add_permintaan_barang'</script>";
-      }
-    } else {
-      echo "<script>alert('Terjadi Kesalahan Input Database'); window.location = 'admin.php?halaman=add_permintaan_barang'</script>";
-    }
-  } else {
-    echo "<script>alert('Gagal Mengirim Data , data detail harus ada !'); window.location = 'admin.php?halaman=add_permintaan_barang'</script>";
-  }
-}
 ?>
 <div class="form-element-list">
   <form id="transaksi_form" method="post">
 
-    <div class="basic-tb-hd">
-      <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-          <div class="form-group">
-            <div class="bootstrap-select fm-cmp-mg">
-              <select id="cari_kode_barang" class="selectpicker" data-live-search="true">
+    <div class="row">
+      <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
 
-                <option value="">Cari Barang</option>
+        <div class="basic-tb-hd">
 
-                <?php
-                $query_jenis = mysqli_query($koneksi, "SELECT * FROM barang ORDER BY kode_barang ASC");
-                while ($data_jenis = mysqli_fetch_array($query_jenis)) {
-                  ?>
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+              <div class="form-group">
+                <div class="bootstrap-select fm-cmp-mg">
+                  <select id="cari_kode_pegawai" class="selectpicker" data-live-search="true">
 
-                <option value="<?= $data_jenis['kode_barang'] ?>"><?= $data_jenis['nama_barang'] ?></option>
+                    <option value="">Cari Pegawai</option>
 
-                <?php } ?>
+                    <?php
+                    $query_pegawai = mysqli_query($koneksi, "SELECT * FROM pegawai ORDER BY kode_pegawai ASC");
+                    while ($data_pegawai = mysqli_fetch_array($query_pegawai)) {
+                      ?>
 
-              </select>
+                    <option value="<?= $data_pegawai['kode_pegawai'] ?>"><?= $data_pegawai['nama_pegawai'] ?></option>
+
+                    <?php } ?>
+
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+              <div class="form-group">
+                <a id="add_more1" name="add_more1" class="btn btn-primary">+</a>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+              <div class="form-group">
+                <div class="bootstrap-select fm-cmp-mg">
+                  <select id="cari_kode_mekanik" class="selectpicker" data-live-search="true">
+
+                    <option value="">Cari Mekanik</option>
+
+                    <?php
+                    $query_mekanik = mysqli_query($koneksi, "SELECT * FROM mekanik ORDER BY kode_mekanik ASC");
+                    while ($data_mekanik = mysqli_fetch_array($query_mekanik)) {
+                      ?>
+
+                    <option value="<?= $data_mekanik['kode_mekanik'] ?>"><?= $data_mekanik['nama_mekanik'] ?></option>
+
+                    <?php } ?>
+
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+              <div class="form-group">
+                <a id="add_more2" name="add_more2" class="btn btn-primary">+</a>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-          <div class="form-group">
-            <a id="add_more" name="add_more" class="btn btn-primary">+</a>
-          </div>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-          <div class="form-group">
-            <button onclick="return confirm('Yakin ingin Mengirim data Permintaan ?')" id="action" type="submit" name="simpan" class="btn btn-primary mr-2">Simpan Pemesanan</button>
-          </div>
-        </div>
-        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-          <div class="form-group">
-            <a href="admin.php?halaman=v_penggajian" class="btn btn-success notika-btn-success">Kembali</a>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <div class="row">
-      <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-        <label>No</label>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <label>Kode Barang</label>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <label>Nama Barang</label>
-      </div>
-      <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-        <label>Jumlah Barang</label>
-      </div>
-    </div>
+        <div class="row">
+          <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+            <label>Kode</label>
+          </div>
+          <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+            <label>Nama</label>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <label>Priode Gaji</label>
+          </div>
+          <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+            <label>Jumlah Hari Kerja</label>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+            <label>Total Gaji</label>
+          </div>
+        </div>
 
-    <div id="span_product_details">
-      <!-- disini isi detail -->
+        <div id="span_product_details">
+          <!-- disini isi detail -->
+
+          <br />
+          <div id="row" class="row">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+              <input type="text" class="form-control" id="kode_pegawai" name="kode_pegawai[]" readonly="" value="">
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+              <input type="text" class="form-control" id="nama_pegawai" name="nama_pegawai[]" readonly="" value="">
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+              <select id="periode_gaji" name="periode_gaji[]" class="form-control selectpicker" data-live-search="true" required="" oninvalid="this.setCustomValidity('Wajib Diisi')" oninput="setCustomValidity('')">
+
+                <option value="">Priode Gaji</option>
+
+                <option value="Januari">Januari</option>
+                <option value="Februari">Februari</option>
+                <option value="Maret">Maret</option>
+
+                <option value="April">April</option>
+                <option value="Mei">Mei</option>
+                <option value="Juni">Juni</option>
+
+                <option value="Juli">Juli</option>
+                <option value="Agustus">Agustus</option>
+                <option value="September">September</option>
+
+                <option value="Oktober">Oktober</option>
+                <option value="Nopember">Nopember</option>
+                <option value="Desember">Desember</option>
+
+              </select>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+              <input type="number" class="form-control" id="jumlah_hari_kerja" name="jumlah_hari_kerja[]" required="" oninvalid="this.setCustomValidity('Wajib Diisi')" oninput="setCustomValidity('')" value="">
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+              <input type="number" class="form-control" id="total_gaji" name="total_gaji[]" max="9999999999" required="" oninvalid="this.setCustomValidity('Wajib Diisi')" oninput="setCustomValidity('')" value="">
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+        <div class="contact-inner">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <div class="basic-tb-hd">
+                <h2 class="text-center">Form Penggajian</h2>
+              </div>
+              <table width="100%">
+                <tr>
+                  <td width="60%">
+                    <h5>Total</h5>
+                  </td>
+                  <td width="40%" style="text-align: right;">
+                    <input type="number" class="form-control" id="total_penggajian" name="total_penggajian" readonly="">
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div style="margin-top: 30px;" class="contact-inner">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+              <table width="100%">
+                <tr>
+                  <td style="padding-top: 15px;" width="60%">
+                    <h5>Bayar</h5>
+                  </td>
+                  <td width="40%" style="text-align: right;"><input type="number" class="form-control" id="bayar" name="bayar" required="" max="9999999999" oninvalid="this.setCustomValidity('Bayar Wajib Diisi')" oninput="setCustomValidity('')" onkeyup="update()" onchange="update()"></td>
+                </tr>
+                <tr>
+                  <td style="padding: 20px 0px;">
+                    <h5>Kembalian</h5>
+                  </td>
+                  <td style="text-align: right; padding: 20px 0px;">
+                    <input type="number" class="form-control" id="kembalian" name="kembalian" readonly="">
+                  </td>
+                </tr>
+
+              </table>
+              <button onclick="return confirm('Yakin ingin Melakukan Penggajian ?')" id="action" type="submit" name="simpan" class="btn btn-primary mr-2">Simpan Penggajian</button>
+              <a href="admin.php?halaman=v_penggajian" class="btn btn-success notika-btn-success">Kembali</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </form>
@@ -131,75 +179,3 @@ if (isset($_POST['simpan'])) {
 
 <!-- untuk ajax -->
 <script src="assets/template2/js/vendor/jquery-3.3.1.min.js"></script>
-
-
-<!-- script logika -->
-<script type="text/javascript">
-  $(document).ready(function() {
-    $('#span_product_details').html('');
-    var count1 = -1;
-    var kode_barang = "";
-
-    // menambah detail pemasokan
-    function add_row(count1, kode_barang, nama_barang) {
-
-      var nomer = count1 + 1;
-
-      $('#span_product_details').append(`
-
-        <br />
-        <div id="row` + count1 + `" class="row">
-          <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-            <p>` + nomer + `</p>
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <input type="text" class="form-control" id="kode_barang` + count1 + `" name="kode_barang[]" readonly="" value="` + kode_barang + `">
-          </div>
-          <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <input type="text" class="form-control" id="nama_barang` + count1 + `" name="nama_barang[]" readonly="" value="` + nama_barang + `">
-          </div>
-          <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-            <div class="form-group">
-              <div class="nk-int-st">
-                <input type="number" id="jumlah_barang` + count1 + `" name="jumlah_barang[]" class="form-control" placeholder="Isi form Jumlah Pesan" required="" max="32000" oninvalid="this.setCustomValidity('Wajib Diisi')" oninput="setCustomValidity('')">
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-            <button id="` + count1 + `" name="remove" class="remove btn btn-primary">-</button>
-          </div>
-        </div>
-        
-      `);
-    }
-
-    // ketika click + maka akan menambah baris
-    $(document).on('click', '#add_more', function() {
-
-      // mengambil data dari select option daftar barang
-      var cari_kode_barang = document.getElementById("cari_kode_barang");
-      var value = cari_kode_barang.options[cari_kode_barang.selectedIndex].value;
-      var value2 = cari_kode_barang.options[cari_kode_barang.selectedIndex].text;
-
-      // validasi cari barang 
-      if (value == "") {
-        alert("Pilih Barang");
-      } else {
-        count1 = count1 + 1;
-        add_row(count1, value, value2);
-
-        document.getElementById("cari_kode_barang").selectedIndex = "0";
-        $('.selectpicker').selectpicker('refresh');
-      }
-
-    });
-
-    // ketika di click - maka akan mengurangi detail pemasokan
-    $(document).on('click', '.remove', function() {
-      var row_no = $(this).attr("id");
-      $('#row' + row_no).remove();
-    });
-
-  });
-</script>
-<!-- script logika -->
