3ce017bf45022417d9056a90a712a3eac5c23f60
zalramli <github.little@gmail.com>
2019-08-21 21:15:51 +0700

    input barang dengan format uang rupiah

--- master/daftar_barang/tambah.php
+++ master/daftar_barang/tambah.php
@@ -21,8 +21,12 @@ if (isset($_POST['simpan'])) {
   $nama_barang = ucfirst($_POST['nama_barang']);
   $stock = $_POST['stock'];
   $stock_limit = $_POST['stock_limit'];
-  $harga_pokok = $_POST['harga_pokok'];
-  $harga_jual = $_POST['harga_jual'];
+  $harga2 = $_POST['harga_pokok'];
+  $harga_string2 =  preg_replace("/[^0-9]/", "", $harga2);
+  $harga_pokok = (int) $harga_string2;
+  $harga = $_POST['harga_jual'];
+  $harga_string =  preg_replace("/[^0-9]/", "", $harga);
+  $harga_jual = (int) $harga_string;
   $query = mysqli_query($koneksi, "INSERT INTO barang VALUES ('$auto_kode','$kode_merk','$kode_satuan','$kode_jenis','$nama_barang','$stock','$stock_limit','$harga_pokok','$harga_jual') ");
   if ($query) {
     echo "<script>alert('Data Berhasil Ditambahkan'); window.location = 'gudang.php?halaman=v_daftarBarang'</script>";
@@ -132,7 +136,7 @@ if (isset($_POST['simpan'])) {
         <label for="">Harga Pokok</label>
         <div class="form-group">
           <div class="nk-int-st">
-            <input type="number" name="harga_pokok" class="form-control" placeholder="Isi form harga pokok barang" required="" max="999999999" oninvalid="this.setCustomValidity('Harga Pokok Wajib Diisi')" oninput="setCustomValidity('')">
+            <input type="text" id="rupiah2" name="harga_pokok" class="form-control" placeholder="Isi form harga pokok barang" required="" oninvalid="this.setCustomValidity('Harga Pokok Wajib Diisi')" oninput="setCustomValidity('')">
           </div>
         </div>
       </div>
@@ -140,7 +144,7 @@ if (isset($_POST['simpan'])) {
         <label for="">Harga Jual</label>
         <div class="form-group">
           <div class="nk-int-st">
-            <input type="number" name="harga_jual" class="form-control" placeholder="Isi form harga jual barang" required="" max="999999999" oninvalid="this.setCustomValidity('Harga Jual Wajib Diisi')" oninput="setCustomValidity('')">
+            <input type="text" id="rupiah" name="harga_jual" class="form-control" placeholder="Isi form harga jual barang" required="" oninvalid="this.setCustomValidity('Harga Jual Wajib Diisi')" oninput="setCustomValidity('')">
           </div>
         </div>
       </div>
@@ -153,5 +157,51 @@ if (isset($_POST['simpan'])) {
 
     </div>
   </form>
+  <script type="text/javascript">
+        var rupiah = document.getElementById('rupiah');
+        rupiah.addEventListener('keyup', function(e){
+            // tambahkan 'Rp.' pada saat form di ketik
+            // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
+            rupiah.value = formatRupiah(this.value);
+        });
+        /* Fungsi formatRupiah */
+        function formatRupiah(angka, prefix){
+            var number_string = angka.replace(/[^,\d]/g, '').toString(),
+            split           = number_string.split(','),
+            sisa            = split[0].length % 3,
+            rupiah          = split[0].substr(0, sisa),
+            ribuan          = split[0].substr(sisa).match(/\d{3}/gi);
+            // tambahkan titik jika yang di input sudah menjadi angka ribuan
+            if(ribuan){
+                separator = sisa ? '.' : '';
+                rupiah += separator + ribuan.join('.');
+            }
+            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+            return prefix == undefined ? rupiah : (rupiah ? + rupiah : '');
+        }
+    </script>
+    <script type="text/javascript">
+        var rupiah2 = document.getElementById('rupiah2');
+        rupiah2.addEventListener('keyup', function(e){
+            // tambahkan 'Rp.' pada saat form di ketik
+            // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
+            rupiah2.value = formatRupiah(this.value);
+        });
+        /* Fungsi formatRupiah */
+        function formatRupiah(angka, prefix){
+            var number_string = angka.replace(/[^,\d]/g, '').toString(),
+            split           = number_string.split(','),
+            sisa            = split[0].length % 3,
+            rupiah2          = split[0].substr(0, sisa),
+            ribuan          = split[0].substr(sisa).match(/\d{3}/gi);
+            // tambahkan titik jika yang di input sudah menjadi angka ribuan
+            if(ribuan){
+                separator = sisa ? '.' : '';
+                rupiah2 += separator + ribuan.join('.');
+            }
+            rupiah2 = split[1] != undefined ? rupiah2 + ',' + split[1] : rupiah2;
+            return prefix == undefined ? rupiah2 : (rupiah2 ? + rupiah2 : '');
+        }
+    </script>
 
 </div>
