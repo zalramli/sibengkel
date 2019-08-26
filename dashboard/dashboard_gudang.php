55e518b4cad88aa4c242430021778f8a41b0b038
zalramli <github.little@gmail.com>
2019-08-26 16:19:23 +0700

    dashboard gudang count

--- dashboard/dashboard_gudang.php
+++ dashboard/dashboard_gudang.php
@@ -1,58 +1,69 @@
-<?php 
+<?php
 $query_permintaan =  mysqli_query($koneksi,"SELECT COUNT(*) as jumlah_permintaan FROM permintaan_barang WHERE status='0'");
 $data_permintaan = mysqli_fetch_array($query_permintaan);
 $query_barang =  mysqli_query($koneksi,"SELECT COUNT(*) as jumlah_barang FROM barang");
 $data_barang = mysqli_fetch_array($query_barang);
+$query_jenis =  mysqli_query($koneksi,"SELECT COUNT(*) as jumlah_jenis FROM jenis_barang");
+$data_jenis = mysqli_fetch_array($query_jenis);
+$query_merk =  mysqli_query($koneksi,"SELECT COUNT(*) as jumlah_merk FROM merk");
+$data_merk = mysqli_fetch_array($query_merk);
+$query_satuan =  mysqli_query($koneksi,"SELECT COUNT(*) as jumlah_satuan FROM satuan");
+$data_satuan = mysqli_fetch_array($query_satuan);
 ?>
-
-    <!-- Icon Cards-->
+<!-- Icon Cards-->
+  <div class="contact-info-area mg-t-30">
     <div class="row">
+      <a style="color:black" href="?halaman=v_permintaan_barang">
       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
-          <div class="website-traffic-ctn">
-            <h2><span class="counter"><?= $data_permintaan['jumlah_permintaan'] ?></span></h2>
-            <p>Total Permintaan Barang</p>
-          </div>
-          <!-- <span style="text-align:right;font-size: 2.3em; color: Tomato;">
-            <i class="fas fa-camera"></i>
-          </span> -->
+        <div class="contact-inner">
+        <div class="contact-inner">
+          <h2><span class="counter"><?= $data_permintaan['jumlah_permintaan'] ?></span></h2>
+          <span><strong>JUMLAH PERMINTAAN BARANG</strong></span>
+        </div>
         </div>
       </div>
+      </a>
+      <a style="color:black" href="?halaman=v_daftarBarang">
       <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30">
-          <div class="website-traffic-ctn">
-            <h2><span class="counter"><?= $data_barang["jumlah_barang"] ?></span></h2>
-            <p>Total Daftar Barang</p>
-          </div>
+        <div class="contact-inner">
+        <div class="contact-inner">
+          <h2><span class="counter"><?= $data_barang['jumlah_barang'] ?></span></h2>
+          <span><strong>JUMLAH DAFTAR BARANG</strong></span>
+        </div>
         </div>
       </div>
+      </a>
     </div>
-    <div class="row">
-      <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-        <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
-          <div class="website-traffic-ctn">
-            <h2>$<span class="counter">40,000</span></h2>
-            <p>Total Online Sales</p>
-          </div>
-          <div class="sparkline-bar-stats3">4,2,8,2,5,6,3,8,3,5,9,5</div>
+    <div class="row mg-t-30">
+      <a style="color:black" href="?halaman=v_jenisBarang">
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <div class="contact-inner">
+        <div class="contact-inner">
+          <h2><span class="counter"><?= $data_jenis['jumlah_jenis'] ?></span></h2>
+          <span><strong>JUMLAH JENIS BARANG</strong></span>
+        </div>
         </div>
       </div>
-      <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-        <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
-          <div class="website-traffic-ctn">
-            <h2>$<span class="counter">40,000</span></h2>
-            <p>Total Online Sales</p>
-          </div>
-          <div class="sparkline-bar-stats3">4,2,8,2,5,6,3,8,3,5,9,5</div>
+      </a>
+      <a style="color:black" href="?halaman=v_merkBarang">
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <div class="contact-inner">
+        <div class="contact-inner">
+          <h2><span class="counter"><?= $data_merk['jumlah_merk'] ?></span></h2>
+          <span><strong>JUMLAH MERK BARANG</strong></span>
+        </div>
         </div>
       </div>
-      <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
-        <div class="wb-traffic-inner notika-shadow sm-res-mg-t-30 tb-res-mg-t-30 dk-res-mg-t-30">
-          <div class="website-traffic-ctn">
-            <h2>$<span class="counter">40,000</span></h2>
-            <p>Total Online Sales</p>
-          </div>
-          <div class="sparkline-bar-stats3">4,2,8,2,5,6,3,8,3,5,9,5</div>
+      </a>
+      <a style="color:black" href="?halaman=v_satuanBarang">
+      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+        <div class="contact-inner">
+        <div class="contact-inner">
+          <h2><span class="counter"><?= $data_satuan['jumlah_satuan'] ?></span></h2>
+          <span><strong>JUMLAH SATUAN BARANG</strong></span>
+        </div>
         </div>
       </div>
+      </a>
     </div>
+  </div>
