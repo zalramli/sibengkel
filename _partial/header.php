37f849362a77374cf79dc687cbc5b9a8f06dbc5c
zalramli <github.little@gmail.com>
2019-09-09 21:15:11 +0700

    ubah nama

--- _partial/header.php
+++ _partial/header.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <div class="logo-area">
-                    <a href="#"><img src="assets/template2/img/logo/logo.png" alt="" /></a>
+                    <a href="#" style="color:white"><h3>Karoseri Cemerlang Jaya</h3></a>
                 </div>
             </div>
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
